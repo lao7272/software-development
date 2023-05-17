adcf2fb9f1ee2179be61d91d26779f88daa35d22
Lao <you@example.com>
2023-05-16 23:21:14 -0300

    Chat logic

--- chat-app/client/script/script.js
+++ chat-app/client/script/script.js
@@ -1,32 +1,51 @@
 
 import { io } from "socket.io-client"
-const socket = io("http://localhost:8080");
 
 const joinRoomButton = document.getElementById("room-button");
-const sendMessageButton = document.getElementById("message-button");
+const messageContainer = document.getElementById("message-container")
 const form = document.getElementById("form");
 const messageInput = document.getElementById("message-input");
 const roomIput = document.getElementById("room-input");
+
+const socket = io("http://localhost:3000");
+const userSocket = io("http://localhost:3000/user", {auth: {token: "test"}})
+socket.on("connect", () => {
+    displayMessage(`You are connected with id: ${socket.id}`)
+});
+
+socket.on("receive-message", (message) => {
+    displayMessage(message)
+})
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const message = messageInput.value;
     const room = roomIput.value;
-
+    
     if(message === "") return;
-
+    
     displayMessage(message);
+    socket.emit("send-message", message, room);
 
     messageInput.value = "";
 });
 
 joinRoomButton.addEventListener("click", () => {
     const room = roomIput.value;
+    socket.emit("join-room", room, message => {
+        displayMessage(message)
+    });
+
+
 });
 
 const displayMessage = (message) => {
     const div = document.createElement("div");
     div.textContent = message;
     div.classList.add(["message"])
-    document.getElementById("message-container").append(div);
+    messageContainer.append(div);
+    scrollToBottom();
+}
+const scrollToBottom = () => {
+    messageContainer.scrollTop = messageContainer.scrollHeight;
 }
--- chat-app/server/server.js
+++ chat-app/server/server.js
@@ -1,22 +1,50 @@
 import express from "express";
 import {createServer} from "http";
 import {Server} from "socket.io";
+import { instrument } from "@socket.io/admin-ui"
+
 
 const app = express();
-
-const server = createServer(app);
-const io = new Server(server, {
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
     cors: {
-        origin: ['http://localhost:8080']
+        origin: ['http://localhost:8080', 'https://admin.socket.io'],
+        credentials: true
     }
 });
-let user;
-io.on("connection", socket => {
-    user = socket.id;
-    console.log(user);
-});
-app.get("/", (req, res) => {
-    res.send(user)
+
+const userIo = io.of('/user');
+userIo.use((socket, next) => {
+    const token = socket.handshake.auth.token;
+    if(token){
+        socket.username = getUsernameFromToken(token);
+        next();
+    } else {
+        next(new Error("Token required"))
+    }
 })
-const PORT = 8080;
-server.listen(PORT, () => console.log(`Running at http://localhost:${PORT}`));
+const getUsernameFromToken = (token) => {
+    return token
+}
+userIo.on("connection", socket => {
+    console.log(`Connected to server with username: ${socket.username}`)
+});
+
+io.on("connection", socket => {
+    console.log(socket.id);
+    socket.on("send-message", (message, room) => {
+        if(room === "") {
+            socket.broadcast.emit("receive-message", message);
+        } else {
+            socket.to(room).emit("receive-message", message);
+        }
+    }); 
+    socket.on("join-room", (room, cb) => {
+        socket.join(room);
+        cb(`Joined ${room}`)
+    })
+});
+
+const PORT = 3000;
+httpServer.listen(PORT, () => console.log(`Running at http://localhost:${PORT}`));
+instrument(io, { auth: false , mode: "development"})
